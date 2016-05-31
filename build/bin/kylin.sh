67e1ada1b7d43dd5506b7cc2c028fec478757e27
lidongsjtu <lidong@apache.org>
2016-05-31 13:20:06 +0800

    update hbase classpath

--- build/bin/kylin.sh
+++ build/bin/kylin.sh
@@ -71,7 +71,7 @@ then
 
     export HBASE_CLASSPATH_PREFIX=${tomcat_root}/bin/bootstrap.jar:${tomcat_root}/bin/tomcat-juli.jar:${tomcat_root}/lib/*:$HBASE_CLASSPATH_PREFIX
     mkdir -p ${KYLIN_HOME}/ext
-    export HBASE_CLASSPATH=$hive_dependency:${KYLIN_HOME}/lib/*:${KYLIN_HOME}/ext/*:${HBASE_CLASSPATH}
+    export HBASE_CLASSPATH=$hive_dependency:${KYLIN_HOME}/lib/*:${KYLIN_HOME}/ext/*:${HBASE_CLASSPATH}:${KYLIN_HOME}/conf
 
     #debug if encounter NoClassDefError
     #hbase classpath
