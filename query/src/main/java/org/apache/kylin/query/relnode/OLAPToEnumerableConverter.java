ec6cc5a904c73d9c8c78e4e60300f908c3fdeb17
fanzhongrui <fanzhongrui@ict.ac.cn>
2016-06-22 16:48:02 +0800

    olaptoEnumerableConverter

--- query/src/main/java/org/apache/kylin/query/relnode/OLAPToEnumerableConverter.java
+++ query/src/main/java/org/apache/kylin/query/relnode/OLAPToEnumerableConverter.java
@@ -44,6 +44,14 @@ import org.apache.kylin.query.routing.QueryRouter;
 import org.apache.kylin.query.schema.OLAPTable;
 
 /**
+ * 当在Calcite中执行一个SQL时，Calcite会解析得到 AST 树，然后再对逻辑执行计划进行优化，
+ *      Calcite的优化规则是基于规则的，在 Calcite 中注册了一些新的 Rule ，
+ *      在优化的过程中会根据这些规则将算子转换为对应的物理执行算子，接下来 Calcite 从上到下一次执行这些算子。
+ *      这些算子都实现了 EnumerableRel 接口，在执行的时候调用 implement 函数
+ */
+
+/**
+ * 在所有Kylin优化之后的查询树中，根节点都是OLAPToEnumerableConverter
  */
 public class OLAPToEnumerableConverter extends ConverterImpl implements EnumerableRel {
 
@@ -62,18 +70,39 @@ public class OLAPToEnumerableConverter extends ConverterImpl implements Enumerab
         return planner.getCostFactory().makeCost(1E100, 0, 0);
     }
 
+    /**
+     * implement函数中首先根据每一个算子中保持的信息构造本次查询的上下文OLAPContext，
+     *      例如根据OLAPAggregateRel 算子获取groupByColumns，
+     *      根据OLAPFilterRel 算子将每次查询的过滤条件转换成TupleFilter
+     *
+     *  然后根据本次查询中使用的维度列（出现在groupByColumns和filterColumns中）、度量信息（aggregations）
+     *      是否有满足本次查询的Cube，如果有则将其保存在 OLAPContext 的 realization 中， 获取数据时需要依赖于它。
+     *
+     *  然后在 rewrite 回调函数中递归调用每一个算子的 implementRewrite函数重新构造每一个算子的参数，
+     *
+     *  最后再调用每一个算子的 implementEnumerable 函数将其转换成EnumerableRef对象，
+     *  这一步相当于将前面生成的物理执行计划再次转换成一个新的物理执行计划。
+     * @param enumImplementor 算子
+     * @param pref
+     * @return
+     */
     @Override
     public Result implement(EnumerableRelImplementor enumImplementor, Prefer pref) {
         // post-order travel children
-        OLAPRel.OLAPImplementor olapImplementor = new OLAPRel.OLAPImplementor();
+        // 后序遍历子树
+        OLAPRel.OLAPImplementor olapImplementor = new OLAPRel.OLAPImplementor();    //olap算子
+        // 构造上下文 olapContext
         olapImplementor.visitChild(getInput(), this);
 
         // find cube from olap context
+        // 从所有OLAP上下文 olapContext 中查找 cube ，
+        // 选择可用IRealization（对于每一个context， 使用 QueryRouter 的 selectRealization函数选择）
         try {
             for (OLAPContext context : OLAPContext.getThreadLocalContexts()) {
                 // Context has no table scan is created by OLAPJoinRel which looks like
                 //     (sub-query) as A join (sub-query) as B
                 // No realization needed for such context.
+
                 if (context.firstTableScan == null) {
                     continue;
                 }
@@ -91,6 +120,7 @@ public class OLAPToEnumerableConverter extends ConverterImpl implements Enumerab
         }
 
         // rewrite query if necessary
+        // 构建RewriteImplementor算子
         OLAPRel.RewriteImplementor rewriteImplementor = new OLAPRel.RewriteImplementor();
         rewriteImplementor.visitChild(this, getInput());
 
@@ -105,6 +135,7 @@ public class OLAPToEnumerableConverter extends ConverterImpl implements Enumerab
             System.out.println(dumpPlan);
         }
 
+        //构建下一个物理执行计划
         return impl.visitChild(this, 0, inputAsEnum, pref);
     }
 
